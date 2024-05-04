70db2e9354f206c78a7535369aa444d1afa8e9dd
jack <144657795+fieje@users.noreply.github.com>
2024-05-04 18:13:35 +0300

    Add files via upload

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("509da4c7-767c-49a4-8541-abd8d5def32d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestFindTrainsInTimeRange()
+        {
+            LinkedList<Train> trainSchedule = new LinkedList<Train>();
+            trainSchedule.AddLast(new Train { TrainNumber = 101, ArrivalTime = new DateTime(2024, 5, 4, 10, 0, 0) });
+            trainSchedule.AddLast(new Train { TrainNumber = 102, ArrivalTime = new DateTime(2024, 5, 4, 11, 30, 0) });
+            trainSchedule.AddLast(new Train { TrainNumber = 103, ArrivalTime = new DateTime(2024, 5, 4, 13, 0, 0) });
+
+            TimeSpan startTime = new TimeSpan(9, 0, 0); 
+            TimeSpan endTime = new TimeSpan(12, 0, 0);  
+
+            LinkedList<Train> trainsInTimeRange = Program.FindTrainsInTimeRange(trainSchedule, startTime, endTime);
+
+            Assert.AreEqual(2, trainsInTimeRange.Count); 
+            foreach (Train train in trainsInTimeRange)
+            {
+                Assert.IsTrue(train.ArrivalTime.TimeOfDay >= startTime && train.ArrivalTime.TimeOfDay <= endTime); 
+            }
+        }
+    }
+
+    public class Train
+    {
+        public int TrainNumber { get; set; }
+        public DateTime ArrivalTime { get; set; }
+    }
+
+    public class Program
+    {
+        public static LinkedList<Train> FindTrainsInTimeRange(LinkedList<Train> trainSchedule, TimeSpan startTime, TimeSpan endTime)
+        {
+            LinkedList<Train> trainsInTimeRange = new LinkedList<Train>();
+
+            foreach (Train train in trainSchedule)
+            {
+                if (train.ArrivalTime.TimeOfDay >= startTime && train.ArrivalTime.TimeOfDay <= endTime)
+                {
+                    trainsInTimeRange.AddLast(train);
+                }
+            }
+
+            return trainsInTimeRange;
+        }
+
+        public static void PrintTrainSchedule(LinkedList<Train> trainSchedule)
+        {
+            foreach (Train train in trainSchedule)
+            {
+                Console.WriteLine($"Train number {train.TrainNumber}, arrival time {train.ArrivalTime}");
+            }
+        }
+
+        public static void PrintTrainScheduleWithPlatform(LinkedList<Train> trainSchedule)
+        {
+            foreach (Train train in trainSchedule)
+            {
+                Console.WriteLine($"Train number {train.TrainNumber}, arrival time {train.ArrivalTime}, platform: {GetPlatformNumber(train.TrainNumber)}");
+            }
+        }
+
+        static int GetPlatformNumber(int trainNumber)
+        {
+            return trainNumber % 3 + 1;
+        }
+    }
+}
